6cf1824a9a7abd01068553b681666e2ed7a03459
Dev Subedi <Devsubedi10@gmail.com>
2025-04-25 12:11:13 +0545

    Remaining Task of day 3

---
+++ lib/DayThree/tasktwo.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TaskTwo());
+}
+
+class TaskTwo extends StatefulWidget {
+  const TaskTwo({super.key});
+
+  @override
+  State<TaskTwo> createState() => _TaskTwoState();
+}
+
+class _TaskTwoState extends State<TaskTwo> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Task Two (Order Menu)'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListItem(
+                      'https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_2560%2Cc_limit/Smashburger-recipe-120219.jpg',
+                      'Burger',
+                      'Wanna grab a bite',
+                      'Purchase',
+                    ),
+                    ListItem(
+                      'https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg?crop=0.8888888888888888xw:1xh;0,0&resize=980:*',
+                      'Pizza',
+                      'Wanna take a slice',
+                      'Buy it',
+                    ),
+                    ListItem(
+                      'https://www.southernliving.com/thmb/TW2iJ6-7F-BAy35Q_EYW5wnIHGI=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Ham_Sandwich_011-1-49227336bc074513aaf8fdbde440eafe.jpg',
+                      'Sandwich',
+                      'Why not today',
+                      'Get it',
+                    ),
+                    ListItem(
+                      'https://www.cookwithmanali.com/wp-content/uploads/2024/01/Veg-Chowmein-1014x1536.jpg',
+                      'Chowmein',
+                      'Just grab your fork',
+                      'Order it',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget ListItem(String url, String title, String desc, String buttonText) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.network(url, height: 200, width: 200),
+          SizedBox(height: 10),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 29),
+          ),
+          SizedBox(height: 10),
+          Text(desc),
+          SizedBox(height: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(minimumSize: Size(200, 50)),
+            onPressed: () {},
+            child: Text(buttonText, style: TextStyle(fontSize: 23)),
+          ),
+        ],
+      ),
+    ),
+  );
+}
